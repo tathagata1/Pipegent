d43aa43bee098fad639168c541c9b9422f58f4d0
Tathagata Mookherjee <tathagata.mookherjee21@gmail.com>
2026-02-15 19:37:36 +0000

    more plugin changes

--- plugins/user_plugins/random_choice/function.py
+++
@@ -1,8 +0,0 @@
-import random
-from typing import List
-
-
-def random_choice(options: List[str]) -> str:
-    if not options:
-        raise ValueError('options cannot be empty')
-    return random.choice(options)
